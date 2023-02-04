a2459a47b4e12a1cc6335fedd3b7d6c2883ba3a4
Oleksandr Nykytin <keeper@ninydev.com>
2023-02-04 13:21:46 +0200

    04 02

---
+++ 20_php/hello.php
@@ -0,0 +1,41 @@
+<?php
+    require ('layout/header.php');
+    include ('layout/footer.php');
+?>
+
+<header> начало работы </header>
+
+<?php
+    echo "<pre>";
+    var_dump($_GET);
+    echo "<hr>";
+    var_dump($_POST);
+    echo "</pre>";
+?>
+
+
+<?php
+//   $c = $a + $b;
+//
+//   $d = 10 / 0;
+?>
+
+<?php
+//    $a = 5;
+//    $b = 5;
+//
+//    echo " + ";
+//    echo $a + $b;
+//    echo "<br>";
+//
+//    echo " . ";
+//    echo $a . " " . $b;
+//    echo "<br>";
+//
+//    echo "$a + $b = " . $a + $b;
+
+?>
+
+<footer>
+    окончание работы
+</footer>
