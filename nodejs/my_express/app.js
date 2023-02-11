5a6c1b5f6bd7c92f54d246ae8480de6211d6b713
Oleksandr Nykytin <keeper@ninydev.com>
2023-02-11 12:49:53 +0200

    11 02

--- nodejs/my_express/app.js
+++ nodejs/my_express/app.js
@@ -1,16 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
 
-var app = express();
+// Создание экземпляра сервера
+let express = require('express');
+let app = express();
 
+// Соединение с базой данных
+require('./config/mongo').connect()
+
+
+// Работа с лог файлами
+let logger = require('morgan');
 app.use(logger('dev'));
+
+
+// Работа с JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+// Работа с куками
+let cookieParser = require('cookie-parser');
 app.use(cookieParser());
+
+
+// Статические файлы
+let path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Маршруты
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let postsRouter = require('./posts/postRouter');
@@ -18,4 +36,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/posts', postsRouter)
 
+
+// передача на запуск сконфигурированного сервера
 module.exports = app;
---
+++ nodejs/my_express/config/mongo.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+exports.connect = () => {
+    console.log (process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then( () => {
+            console.log('Connect to mongodb ok')
+        })
+        .catch( (err) => {
+            console.log('db connect err')
+            console.log(err)
+            process.exit(500)
+        })
+}
